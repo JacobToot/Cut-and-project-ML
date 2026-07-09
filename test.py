a1fbcad42cc232a954ae150b9bf580a461f23b0d
Jacob Toot <jacobtoot@icloud.com>
2026-07-09 12:30:43 +0200

    Delete test.py
    Unnecessary test script. Removed

--- test.py
+++
@@ -1,3 +0,0 @@
-import torch
-state_dict = torch.load("/Users/jacobtoot/Documents/Documents - Jacob’s MacBook Pro/GitHub/cut-and-project-ML/results/npe_1d/npe_1d_thesis/best_weights.pt", map_location="cpu")
-print(list(state_dict.keys())[:10])
